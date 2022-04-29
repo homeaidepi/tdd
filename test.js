2f1bc4c304c07ce99f389c9ebd207866ead6e3f6
Vandy <matthew.vandergrift@homeaidepi.com>
2022-04-29 10:39:33 -0400

    update refs

--- test.js
+++ test.js
@@ -7,27 +7,45 @@ if (typeof module !== 'undefined' && module.exports) { // check we're server-sid
   var getChange = require('./change.js'); // load our getChange method
 }
 
+// Positive Test
 test('getChange(1,1) should equal [] - an empty array', function(assert) {
   var result = getChange(1, 1); //no change/coins just an empty array
   var expected = [];
   assert.deepEqual(result, expected);
 });
 
+// Positive Test
 test('getChange(215, 300) should return [50, 20, 10, 5]', function(assert) {
   var result = getChange(215, 300); // expect an array containing [50,20,10,5]
   var expected = [50, 20, 10, 5];
   assert.deepEqual(result, expected);
 });
 
+// Negative Test
+test('getChange(null,null) should equal [] - an empty array', function(assert) {
+  var result = getChange(null, null); //no change/coins just an empty array
+  var expected = [];
+  assert.deepEqual(result, expected);
+});
+
+// Positive Test
 test('getChange(486, 600) should equal [100, 10, 2, 2]', function(assert) {
   var result = getChange(486, 600);
   var expected = [100, 10, 2, 2];
   assert.deepEqual(result, expected);
 });
 
+// Positive Test
 test('getChange(12, 400) should return [200, 100, 50, 20, 10, 5, 2, 1]', function(assert) {
   var result = getChange(12, 400);
   var expected = [200, 100, 50, 20, 10, 5, 2, 1];
+  assert.deepEqual(result, expected);
+});
+
+// Negative Test
+test('getChange(obj,obj w/props) should equal [] - an empty array', function(assert) {
+  var result = getChange({}, {"a":"b"}); //no change/coins just an empty array
+  var expected = [];
   assert.deepEqual(result, expected);
 });
 
